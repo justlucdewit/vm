745ee1a7b89c29676f0e833a267b8ed7122919d0
Luc de Wit <lucdewit@Lucs-MacBook-Pro.local>
2025-09-17 15:32:10 +0200

    Added support for 64 bit systems when aplicable

--- src/assembler/assemble.h
+++ src/assembler/assemble.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "parser.h"
+#include <stdint.h>
+#include <inttypes.h>
 
 // Converts a register like a b c or d to a byte value 0 1 2 3
 uint8_t register_to_byte(char* r) {
@@ -13,14 +15,15 @@ uint8_t register_to_byte(char* r) {
     return 0xFF;
 }
 
+uintptr_t string_to_uintptr(char* s) {
+	return (uintptr_t) strtoumax(s, NULL, 0);
+}
+
 void assemble_parseresult(ParserResult parserResult, char* output_filename) {
     // Buffer to store the output bytes
     uint8_t* output_bytes = create_list_of(uint8_t, output_bytes, 100);
 
     uint8_t instruction_to_opcode_map[] = {};
-
-    // MOV instructions
-    instruction_to_opcode_map[IT_MOV_REG_INT] = 0x01;
 
     // Loop trough each instruction and convert to bytes
     for (uint32_t i = 0; i < parserResult.instruction_count; i++) {
@@ -29,8 +32,7 @@ void assemble_parseresult(ParserResult parserResult, char* output_filename) {
         // Check what instruction it is
         if (inst.type == IT_MOV_REG_INT) { // 0x00 [register] [int32]
             // Opcode for MOV_REG_INT
-            uint8_t opcode = instruction_to_opcode_map[IT_MOV_REG_INT];
-            insert_at_end(uint8_t, output_bytes, opcode);
+            insert_at_end(uint8_t, output_bytes, inst.type);
 
             // Register
             char* reg_str = inst.arguments[0];
@@ -38,11 +40,24 @@ void assemble_parseresult(ParserResult parserResult, char* output_filename) {
             insert_at_end(uint8_t, output_bytes, reg_byte);
 
             // Integer (4 bytes, big endian)
-            int32_t int_value = atoi(inst.arguments[1]);
-            insert_at_end(uint8_t, output_bytes, (int_value >> 24) & 0xFF);
-            insert_at_end(uint8_t, output_bytes, (int_value >> 16) & 0xFF);
-            insert_at_end(uint8_t, output_bytes, (int_value >> 8) & 0xFF);
-            insert_at_end(uint8_t, output_bytes, int_value & 0xFF);
+            uintptr_t int_value = string_to_uintptr(inst.arguments[1]);
+
+            for (size_t i = 0; i < sizeof(uintptr_t); ++i) {
+                size_t shift = (sizeof(uintptr_t) - 1 - i) * 8;
+                uint8_t byte = (uint8_t)((int_value >> shift) & 0xFF);
+                insert_at_end(uint8_t, output_bytes, byte);
+            }
+        } else if (inst.type == IT_MOV_REG_REG) {
+            // Opcode for MOV_REG_REG
+            insert_at_end(uint8_t, output_bytes, inst.type);
+
+            // Register
+            uint8_t reg_byte = register_to_byte(inst.arguments[0]);
+            insert_at_end(uint8_t, output_bytes, reg_byte);
+
+            // Second Register
+            reg_byte = register_to_byte(inst.arguments[1]);
+            insert_at_end(uint8_t, output_bytes, reg_byte);
         } else {
             printf("[error] Unknown instruction type %d\n", inst.type);
         }
